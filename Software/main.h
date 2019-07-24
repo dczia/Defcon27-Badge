73c7d112858a96d0323973b2b55d487302acf2b7
Ancient123 <mrichmond@xpansiv.com>
2019-07-24 16:08:38 -0500

    Cleanup main function and pull stuff into functions

--- Software/main.h
+++ Software/main.h
@@ -13,7 +13,9 @@
 #define MAIN_H_
 
 #define VERSION "1.00"
-
-
-
 #endif /* MAIN_H_ */
+void audio_off();
+uint8_t button_handler(uint8_t mode);
+void startup_sequence();
+uint8_t tof_pitch(uint8_t prevRange);
+uint8_t tof_volume(uint8_t prevRange);
